c1af5170f16b67543bc83e266966bb4607f02833
Braber01 <ben.raber89@gmail.com>
2017-10-05 09:42:11 -0500

    Added Card.ts

---
+++ src/ts/Card.ts
@@ -0,0 +1,99 @@
+﻿import { CardRank, CardSuit, CardColor,GameTypes } from './CardEnums';
+import * as THREE from 'three';
+
+/**
+ * The Main Card class. Contains basic data about each card.
+ * Can be one of four suits and have one of 13 values
+ */
+export default class Card {
+    //public node: HTMLElement
+
+    /**
+     * The value of the Card. 2-10, Jack, Queen,King,Ace
+     * @type {CardRank}
+     */
+    public rank: CardRank;
+
+    /**
+     * The suit of the card
+     * Clubs, Diamonds, Hearts Spades
+     * @type {CardSuit}
+     */
+    public suit: CardSuit;
+
+    /**
+     * The Callback to run when this card is selected.
+     * @type {Function}
+     */
+    private selectCallback: Function;
+
+    /**
+     * Has this card been revealed?
+     * @type {boolean}
+    */
+    public revealed: boolean;
+
+    /**
+     *? Is this card Currently in a pile?
+     *! Set "from outside" when card is being moved
+     * @type {boolean}
+    */
+    public isInPile: boolean = false;
+
+    /**
+     * Is this card currently in a foundation?
+     * Set "from outside" when card is being moved.
+     * @type {boolean}
+    */
+    public isInFoundation: boolean;
+
+    public gameType: GameTypes;
+
+
+    /**
+     * Create a new card
+     * @param {CardRank} rank
+     * @param {CardSuit} suit
+     * @param {Function} selectCallback
+     * @param {GameTypes} gameType
+    */
+    constructor(rank: CardRank, suit: CardSuit, selectCallback: Function,gameType = GameTypes.KLONDIKE) {
+        this.rank = rank;
+        this.suit = suit;
+        this.selectCallback = selectCallback;
+    }
+
+    /**
+     * Get the color value of this card.
+     * @returns {CardColor}
+     */
+    public color(): CardColor {
+        return (this.suit == CardSuit.HEARTS || this.suit == CardSuit.DIAMONDS) ? CardColor.RED : CardColor.BLACK;
+    }
+
+    /**
+     * TODO: setup thee.js elements
+     * not sure of the types yet
+    */
+    setupThree(): any { }
+
+    isSelectable(): boolean {
+        return this.revealed === true;
+    }
+
+    updateThree(): any { }
+
+    acceptsSibling(card: Card): boolean {
+        if (this.gameType == GameTypes.KLONDIKE) {
+            if (this.isInFoundation) {
+                return card.suit === this.suit && card.rank === (this.rank + 1);
+            }
+            if (this.isInPile) {
+                return card.color() !== this.color() && card.rank === (this.rank - 1);
+            }
+            //TODO: Add Additonal Game Rules below
+
+            return false;
+        }
+    }
+}
