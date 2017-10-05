682ab6eff9c0d741f8bcc595215952a876eab466
Braber01 <ben.raber89@gmail.com>
2017-10-05 11:51:53 -0500

    Added Selected.ts

---
+++ src/ts/Selected.ts
@@ -0,0 +1,9 @@
+﻿import Card from "./Card";
+import { GameTypes, BuildRules } from "./CardEnums";
+//no node elements so no THREE.js needed
+
+export default class Selected {
+    private card: Card;
+    protected stack: Card[] = [];
+    public dragging: boolean = false;
+}
